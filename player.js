a9209670eb1dafb99d3d105b9f461e43383c90c0
Adin Smith <adins1761@gmail.com>
2026-01-21 18:04:47 -0800

    everything kinda works now

--- player.js
+++ player.js
@@ -4,41 +4,96 @@ class Player {
         this.spritesheet = ASSET_MANAGER.getAsset("./Kirby2.png")
         this.animations = {
             "idle": new Animator(this.spritesheet, 7, 10, 22, 20, 1, 0.2, false, false),
-            "walk-right": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.2, false, true),
-            "walk-left": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.2, false, true),
+            "walk-right": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.1, false, true),
+            "walk-left": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.1, false, true),
             "duck": new Animator(this.spritesheet, 31, 10, 26, 20, 1, 0.2, false, false),
-            "jump": new Animator(this.spritesheet, 31, 10, 26, 20, 1, 0.2, false, false)
+            "jump-right": new Animator(this.spritesheet, 720, 8, 21, 21, 1, 0.2, false, false),
+            "jump-left": new Animator(this.spritesheet, 720, 8, 21, 21, 1, 0.2, false, false)
         };
 
         this.x = 500;
         this.y = 500;
         this.speed = 0;
         this.jump = 0;
+
+        this.velocity = { x : 0, y : 0};
+        this.fallAcc = 562.5;
     };
 
     update() {
-        this.x += this.speed * this.game.clockTick;
-        if (this.x > 1024) this.x = 0;
+        // this.x += this.speed * this.game.clockTick;
+        // if (this.x > 1024) this.x = 0;
 
-        this.y += this.jump * this.game.clockTick;
-        const keys = this.game.keys;
+        // this.y += this.jump * this.game.clockTick;
+        // const keys = this.game.keys;
         
-        this.animator = this.animations["idle"];
-        this.speed = 0;
-        this.jump = 0;
+        // this.animator = this.animations["idle"];
+        // this.speed = 0;
+        // this.jump = 0;
 
-        if (keys["w"] || keys["ArrowUp"]) {
-            this.animator = this.animations["jump"]; 
-            this.jump = -100;
-        }
-        if (keys["s"] || keys["ArrowDown"]) this.animator = this.animations["duck"]; 
+        // if (keys["w"] || keys["ArrowUp"]) {
+        //     this.animator = this.animations["jump"]; 
+        //     this.jump = -100;
+        // }
+        // if (keys["s"] || keys["ArrowDown"]) this.animator = this.animations["duck"]; 
+        // if (keys["a"] || keys["ArrowLeft"]) {
+        //     this.animator = this.animations["walk-left"];
+        //     this.speed = -100;
+        // } 
+        // if (keys["d"] || keys["ArrowRight"]) {
+        //     this.animator = this.animations["walk-right"];
+        //     this.speed = 100;
+        // }
+        const TICK = this.game.clockTick;
+        const keys = this.game.keys;
+
+        // --- Horizontal movement ---
+        this.speed = 0;
         if (keys["a"] || keys["ArrowLeft"]) {
+            this.speed = -250;
             this.animator = this.animations["walk-left"];
-            this.speed = -100;
-        } 
-        if (keys["d"] || keys["ArrowRight"]) {
+        } else if (keys["d"] || keys["ArrowRight"]) {
+            this.speed = 250;
             this.animator = this.animations["walk-right"];
-            this.speed = 100;
+        } else {
+            this.animator = this.animations["idle"];
+        }
+
+        this.x += this.speed * TICK;
+        if (this.x > 1024) this.x = 0;
+        if (this.x < 0) this.x = 1024;
+
+        // --- Ground check ---
+        const groundY = 500;
+        const onGround = this.y >= groundY;
+
+        // --- Jump input ---
+        if ((keys["w"] || keys["ArrowUp"]) && onGround) {
+            this.velocity.y = -500; // jump impulse
+        }
+
+        // --- Gravity ---
+        this.velocity.y += this.fallAcc * TICK;
+        this.y += this.velocity.y * TICK;
+
+        // --- Ground collision ---
+        if (this.y > groundY) {
+            this.y = groundY;
+            this.velocity.y = 0;
+        }
+
+        // --- Jump animation ---
+        if (!onGround) {
+            if (keys["a"] || keys["ArrowLeft"]) {
+                this.animator = this.animations["jump-left"];
+            } else {
+                this.animator = this.animations["jump-right"];
+            }
+        }
+
+        // --- Duck ---
+        if (keys["s"] || keys["ArrowDown"]) {
+            this.animator = this.animations["duck"];
         }
     }
 
@@ -46,7 +101,12 @@ class Player {
         if (this.animator == this.animations["walk-left"]) {
             ctx.save();
             ctx.scale(-1, 1);
-            this.animator.drawFrame(this.game.clockTick, ctx, -(this.x) - 22, this.y);
+            this.animator.drawFrame(this.game.clockTick, ctx, -(this.x) - 100, this.y);
+            ctx.restore();
+        } else if (this.animator == this.animations["jump-left"]) {
+            ctx.save();
+            ctx.scale(-1, 1);
+            this.animator.drawFrame(this.game.clockTick, ctx, -(this.x) - 100, this.y);
             ctx.restore();
         } else {
             this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
