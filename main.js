13dbe750a8e562b33b2c76dc6cf5dc0fe7e87944
Adin Smith <adins1761@gmail.com>
2026-01-21 16:12:02 -0800

    fixed sprites and animations

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./Kirby.png");
+ASSET_MANAGER.queueDownload("./Kirby2.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
--- player.js
+++ player.js
@@ -1,36 +1,55 @@
 class Player {
     constructor(game) {
         this.game = game;
-        this.spritesheet = ASSET_MANAGER.getAsset("./Kirby.png")
+        this.spritesheet = ASSET_MANAGER.getAsset("./Kirby2.png")
         this.animations = {
             "idle": new Animator(this.spritesheet, 7, 10, 22, 20, 1, 0.2, false, false),
             "walk-right": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.2, false, true),
-            "walk-left": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.2, true, true),
+            "walk-left": new Animator(this.spritesheet, 253, 10, 22, 20, 10, 0.2, false, true),
             "duck": new Animator(this.spritesheet, 31, 10, 26, 20, 1, 0.2, false, false),
             "jump": new Animator(this.spritesheet, 31, 10, 26, 20, 1, 0.2, false, false)
         };
 
-        this.animator = this.animations["idle"]
-
         this.x = 500;
         this.y = 500;
         this.speed = 0;
+        this.jump = 0;
     };
 
     update() {
         this.x += this.speed * this.game.clockTick;
-        if (this.x > 1024) this.x = 0
+        if (this.x > 1024) this.x = 0;
 
+        this.y += this.jump * this.game.clockTick;
         const keys = this.game.keys;
         
-        if (keys["w"] || keys["ArrowUp"]) this.animator = this.animations["jump"];
-        if (keys["s"] || keys["ArrowDown"]) this.animator = this.animations["duck"];
-        if (keys["a"] || keys["ArrowLeft"]) this.animator = this.animations["walk-left"];
-        if (keys["d"] || keys["ArrowRight"]) this.animator = this.animations["walk-right"];
-    };
+        this.animator = this.animations["idle"];
+        this.speed = 0;
+        this.jump = 0;
+
+        if (keys["w"] || keys["ArrowUp"]) {
+            this.animator = this.animations["jump"]; 
+            this.jump = -100;
+        }
+        if (keys["s"] || keys["ArrowDown"]) this.animator = this.animations["duck"]; 
+        if (keys["a"] || keys["ArrowLeft"]) {
+            this.animator = this.animations["walk-left"];
+            this.speed = -100;
+        } 
+        if (keys["d"] || keys["ArrowRight"]) {
+            this.animator = this.animations["walk-right"];
+            this.speed = 100;
+        }
+    }
 
     draw(ctx) {
-        
-        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        if (this.animator == this.animations["walk-left"]) {
+            ctx.save();
+            ctx.scale(-1, 1);
+            this.animator.drawFrame(this.game.clockTick, ctx, -(this.x) - 22, this.y);
+            ctx.restore();
+        } else {
+            this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        }
     };
 }
